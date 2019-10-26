d747886d713b6e78c2d9edf3f2bfcedd63e60654
logan-r <logan.ralston@mail.mcgill.ca>
2019-10-26 05:07:17 -0400

    Updates

---
+++ precache-manifest.1cfcdd04edc0f9c72ca1e8f257c81822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4388ee0404a3857f937b623c576a089b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14ec0fc4479ff0186b8f",
+    "url": "/static/css/main.eff11c92.chunk.css"
+  },
+  {
+    "revision": "c75feb3df91695d2ff83",
+    "url": "/static/js/2.775d46b9.chunk.js"
+  },
+  {
+    "revision": "14ec0fc4479ff0186b8f",
+    "url": "/static/js/main.8008be1d.chunk.js"
+  },
+  {
+    "revision": "79d543e375e79c08d21a",
+    "url": "/static/js/runtime-main.7cbc03a8.js"
+  }
+]);
