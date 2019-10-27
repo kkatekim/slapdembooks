a1b1ea2ca6b57ce0805b9eaf09ae3feda71b39b0
logan-r <logan.ralston@mail.mcgill.ca>
2019-10-27 01:33:35 -0400

    Fix netlify deploy:

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import Friends from './pages/Friends/Friends';
 
 ReactDOM.render(
     <FirebaseContext.Provider value={new Firebase()}>
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
             <Route exact path='/' component={Home} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/logout' component={Logout} />
