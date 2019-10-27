844ffc91de2123a14a9ebb841010f695483725e6
logan-r <logan.ralston@mail.mcgill.ca>
2019-10-27 01:13:00 -0400

    Updates

---
+++ precache-manifest.d01d689d607fe7de352df00a59714234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e291de7ab96a2e43322db7e39171bb8b",
+    "url": "/slapdembooks/index.html"
+  },
+  {
+    "revision": "a417bbc2676f455bfb68",
+    "url": "/slapdembooks/static/css/main.d039dd24.chunk.css"
+  },
+  {
+    "revision": "2ea2969da7279a8311dd",
+    "url": "/slapdembooks/static/js/2.349187cc.chunk.js"
+  },
+  {
+    "revision": "a417bbc2676f455bfb68",
+    "url": "/slapdembooks/static/js/main.e7a8e38f.chunk.js"
+  },
+  {
+    "revision": "0134d2f2bf131a72bc1e",
+    "url": "/slapdembooks/static/js/runtime-main.7f085979.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e8eec3d60142d5cb7495b6743938151.js"
+  "/slapdembooks/precache-manifest.d01d689d607fe7de352df00a59714234.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/slapdembooks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
